3774f91fdfbd8f3046ce4c852dc625e978a490cd
mdshababanwer06-afk <mdshababanwer06@gmail.com>
2026-09-01 23:28:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.43%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        curr_s = s
+        n = len(curr_s)
+        for i in range(0,n):
+            if curr_s == goal:
+                return True
+            curr_s = curr_s[-1]+curr_s[:-1]
+        return False
